a248e212aa5ba768bcc1b9fe8135412c1f6a74e7
moluanshanhe <1359493339@qq.com>
2021-08-06 12:12:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.99/g' package/base-files/files/bin/config_generate
+sed -i 's/${vendorid:+-V "$vendorid"} \/-V ""} \/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
